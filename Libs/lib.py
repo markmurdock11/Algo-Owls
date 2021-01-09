4b8c8dd11c54e96d1f4a50adf6a7894e267f8c13
markmurdock11 <markmurdock11@gmail.com>
2021-01-09 10:35:10 -0600

    good data pipeline

--- Libs/lib.py
+++ Libs/lib.py
@@ -6,9 +6,9 @@ import pandas as pd
 import numpy as np
 import alpaca_trade_api as tradeapi
 
-def fetch_data(ticker):
+def fetch_ohlcv(ticker, start_date= "2020-01-01", end_date = "2021-01-01"):
     """Function to automatically pull closing price data from Alpaca"""
-    """  example syntax: fetch_data(["TSLA"])  """
+    """  example syntax: get_dat_data("TSLA")  """
     
     # Insert Personal Secret Keys (check .ENV to match syntax if error occurs)
     alpaca_api_key = os.getenv("APCA_API_KEY_ID")
@@ -20,7 +20,8 @@ def fetch_data(ticker):
         api_version= "v2")
     
     # Backdating the Data to the first day of 2020
-    back_date = pd.Timestamp("2020-01-01", tz="America/New_York").isoformat()
+    back_date = pd.Timestamp(start_date, tz="America/New_York").isoformat()
+    end_date = pd.Timestamp(end_date, tz="America/New_York").isoformat()
 
     # Choosing the ticker we want (need to figure out how to make this the input)
     #ticker = ["TSLA"]
@@ -32,20 +33,12 @@ def fetch_data(ticker):
     df_ticker = alpaca.get_barset(
         ticker,
         timeframe,
-        start=back_date
+        start=back_date,
+        end = end_date
         ).df
     
-    # Selecting only the closing price for the ticker
-    closing_price = pd.DataFrame()
+    return df_ticker[ticker]
 
-    for time_period in ticker:
-        closing_price[time_period] = df_ticker[time_period]["close"]
-
-    closing_price.dropna()
-
-    closing_price.rename(columns= {closing_price.columns[0]: "close"}, inplace=True)
-    closing_price = pd.DataFrame(closing_price)
-    return closing_price
 
     # Bollinger Band function to capture signal
 def bollinger_band_signal_generator(dataframe_name, closing_price_column_name = 'close', bollinger_band_window = 20, num_standard_deviation = 2):
